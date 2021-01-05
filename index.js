e5835a23bd6cf280e98ce9c84248ed5d4eadb78b
Kamal <darkxnovaz@gmail.com>
2021-01-05 20:31:21 +0200

    implement general input only procedures function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import {config} from './config.js'
-
 const express = require('express');
 const mssql = require('mssql');
 const app = express();
 const port = 3000;
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
+
+var read = fs.readFileSync("config.json");
+var config = JSON.parse(read);
 
 var conn = new mssql.ConnectionPool(config);
-var connection = conn.connect();
-var pool = new mssql.Request(conn);
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -28,38 +28,66 @@ app.get('/registration', function(req, res) {
     res.render('registration');
 });
 
-app.post('/registration', function(req, res) {
-    connection.then(() => {
-        pool.input('first_name', mssql.VarChar(20), req.body.firstName);
-        pool.input('last_name', mssql.VarChar(20), req.body.lastName);
-        pool.input('password', mssql.VarChar(20), req.body.password);
-        pool.input('email', mssql.VarChar(50), req.body.email);
-        pool.input('gender', mssql.Bit, req.body.gender);
-        pool.input('address', mssql.VarChar(10), req.body.address);
-        pool.execute('studentRegister', (err, result) => {
-            console.log(result + "result");
-        })
-    }).catch(err => {
-        console.log(err);
-    })
+app.post('/register', function(req, res) {
+    conn.connect();
+    var procedure = ["studentRegister", null, false, true];
+    runProcedure(req.body, procedure);
+    res.redirect('/login');
 });
 
-function runStoredProcedure(req) {
-    console.log(req.body);
-    return connection.then((pool) => {
-        pool.request()
-        .input('first_name', mssql.VarChar(20), req.body.firstName)
-        .input('last_name', mssql.VarChar(20), req.body.lastName)
-        .input('password', mssql.VarChar(20), req.body.password)
-        .input('email', mssql.VarChar(50), req.body.email)
-        .input('gender', mssql.Bit, req.body.gender)
-        .input('address', mssql.VarChar(10), req.body.address)
-        .execute('studentRegister', (err, result) => {
-            console.log(result)
-        })
-    }).catch(err => {
-        console.log(err);
-    })
+function runProcedure(body, proc) {
+    var inputs = []
+    for (var key in body){
+        input = [key, body[key], false, false];
+        inputs.push(input);
+    }
+    inputs.push(proc);
+    // inputs[input][3] corresponds to a output statement
+    // inputs[input][4] corresponds to an execute statement
+    conn.connect().then(() => {
+        var request = new mssql.Request(conn);
+        for (input in inputs){
+            if (!inputs[input][2] && !inputs[input][3]){
+                request.input(inputs[input][0], inputs[input][1])
+            }
+            else if(inputs[input][2]){
+                request.output(inputs[input][0], inputs[input][1])
+            }
+            else{
+                request.execute(inputs[input][0]).then(function (recordSet) {
+                    console.log(recordSet + " this is the record set");
+                    conn.close();
+                }).catch(function (err) {
+                    console.log(err + " this is error1");
+                    conn.close();
+                });
+                break;
+            }
+        }
+    }).catch(function (err) {
+        console.log(err + " this is error2");
+    });
+}
+
+function runStudentRegister(req) {
+    conn.connect().then(function () {
+        var request = new mssql.Request(conn);
+        request.input('first_name', req.body.firstName);
+        request.input('last_name', req.body.lastName);
+        request.input('password', req.body.password);
+        request.input('email', req.body.email);
+        request.input('gender', req.body.gender);
+        request.input('address', req.body.address);
+        request.execute("studentRegister").then(function (recordSet) {
+            console.log(recordSet + " this is the record set");
+            conn.close();
+        }).catch(function (err) {
+            console.log(err + " this is error1");
+            conn.close();
+        });
+    }).catch(function (err) {
+        console.log(err + " this is error2");
+    });
 }
 
 app.listen(port, () => {
